3534c28772b69aa0058d336ffebb3dc63ec585bf
Mary <32392607+MartFrida@users.noreply.github.com>
2025-03-16 14:40:32 +0100

    adaptated pictures

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -14,40 +14,44 @@ export const InfoSection = ({ title, description, items, callToAction, imageUrl 
   };
 
   return (
-    <div>
-      <h2 className={`${theme.primary} text-2xl p-4 rounded-lg  hover:scale-101 transition duration-300`}>
+    <div className="relative">
+      <h2 className={`${theme.primary} text-2xl p-4 rounded-lg hover:scale-101 transition duration-300`}>
         {title}
       </h2>
-      <div className="flex flex-col lg:flex-row my-4 gap-4 ">
-        <div className="grow-3 pl-4">
-          <p className="leading-relaxed tracking-wide text-gray-700 ">
-            {description}
-          </p>
-          <ul className="list-disc text-gray-700 list-none">
-            {items.map((item, index) => (
-              <li key={index}> {item}</li>
-            ))}
-          </ul>
-        </div>
-        {imageUrl &&
-          <div className="grow-3 justify-items-end">
+
+      <div className="my-4 gap-4">
+        {imageUrl && (
+          <div className="lg:float-right w-100% m-0 lg:ml-4 mb-2 lg:w-[300px] h-64">
             <img
               src={`${imageUrl}`}
               alt='Training Image'
-              className='rounded-lg shadow-lg max-h-[59vh] object-cover'
+              className='rounded-lg shadow-lg w-full h-full object-cover'
             />
           </div>
-        }
+        )}
+
+        <p className="leading-relaxed tracking-wide text-gray-700">
+          {description}
+        </p>
+        <ul className="list-disc text-gray-700 list-none">
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
+
       <p className="leading-relaxed tracking-wide mb-4 text-gray-700">
         {callToAction}
       </p>
-      <div className='flex justify-end w-full '>
-        <button className="bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-600 hover:to-gray-800 text-white"
+
+      <div className="flex justify-end w-full">
+        <button
+          className="bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-600 hover:to-gray-800 text-white"
           onClick={(ev) => {
-            handleClick(ev)
-            setIsModalOpen(true)
-          }}>
+            handleClick(ev);
+            setIsModalOpen(true);
+          }}
+        >
           Contáctenos
         </button>
         <ContactModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} sectionID={idElem} />

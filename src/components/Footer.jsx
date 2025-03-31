c946db1850aaafeb59adc678a47ab861db9e84a6
Mary <32392607+MartFrida@users.noreply.github.com>
2025-03-31 22:04:59 +0200

    fin de lang of home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { theme } from "../helpers/theme";
 import { Link } from "react-router-dom";
 import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className="bg-gray-900 text-white p-6">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center text-center md:text-left">
@@ -16,17 +18,17 @@ const Footer = () => {
 
         {/* Сервисы */}
         <div className="mb-6 md:mb-0">
-          <h3 className="text-lg font-semibold">Servicios</h3>
+          <h3 className="text-lg font-semibold">{t("servicios")}</h3>
           <ul className="mt-2 space-y-1">
-            <li><Link to="/entrenador-personal" className="hover:underline">Personalized Training</Link></li>
-            <li><Link to="/clases-infantiles" className="hover:underline">Clases Infantiles</Link></li>
-            <li><Link to="/nutrition" className="hover:underline">Nutrition & Diet</Link></li>
+            <li><Link to="/entrenador-personal" className="hover:underline">{t("personalTraining")}</Link></li>
+            <li><Link to="/clases-infantiles" className="hover:underline">{t("clasesInfantiles")}</Link></li>
+            <li><Link to="/nutrition" className="hover:underline">{t("nutrition")}</Link></li>
           </ul>
         </div>
 
         {/* Контакты */}
         <div>
-          <h3 className="text-lg font-semibold">Contactos</h3>
+          <h3 className="text-lg font-semibold">{t("actionBtn")}</h3>
           <ul className="mt-2 space-y-1">
             <li className="flex justify-center md:justify-start items-center">
               <a href="tel:+34696869949" className="flex flex-row items-center"> <FaPhone className="mr-2" /> +34 696 869 949</a>
@@ -49,7 +51,7 @@ const Footer = () => {
       </div>
 
       <div className="text-center mt-6 border-t border-gray-700 pt-4">
-        <p>&copy; {new Date().getFullYear()} CENTRO INTERNACIONAL DE ENTRENADORES PERSONALES Y TERAPIAS INTEGRALES DE LA SALUD</p>
+        <p>&copy; {new Date().getFullYear()} {t("home.title")}</p>
         {/* <p> Reservados todos los derechos</p> */}
       </div>
     </footer>

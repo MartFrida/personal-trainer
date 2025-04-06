63f98f47cb89ed6d5740f5ac1304afda2946eff1
Mary <32392607+MartFrida@users.noreply.github.com>
2025-04-06 22:50:29 +0200

    structura for articles

--- src/components/Blog/Article.jsx
+++ src/components/Blog/Article.jsx
@@ -8,11 +8,11 @@ const Article = ({ title, content }) => {
   // Функция для вставки изображения в контент
   const renderImage = (content) => {
     // Используем регулярное выражение для поиска маркера #image: и замены его на тег <img>
-    const imageRegex = /#image:\s*(https?:\/\/[^\s]+)/g;
+    const imageRegex = /#image:\s*(https?:\/\/[^\s]+)\s*"([^"]+)"/g;
 
     // Заменяем маркер на тег <img> с соответствующим URL
-    return content.replace(imageRegex, (match, url) => {
-      return `<img src="${url}" alt="Image" class="inline-block max-w-xs float-left mr-4 my-2 rounded-lg"/>`;
+    return content.replace(imageRegex, (match, url, altText) => {
+      return `<img src="${url}" alt="${altText}" class="inline-block max-w-xs float-left mr-4 my-2 rounded-lg"/>`;
     });
   };
 
@@ -21,7 +21,7 @@ const Article = ({ title, content }) => {
 
   return (
     <div className={`${theme.text} relative text-xl flex flex-col gap-2`}>
-      <h2 className="text-2xl p-4 rounded-lg">
+      <h2 className="text-4xl p-4 rounded-lg">
         {title}
       </h2>
       <ReactMarkdown

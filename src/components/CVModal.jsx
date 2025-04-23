8a267f8256acda05cfb3f1f247760f956f672f0b
Mary <32392607+MartFrida@users.noreply.github.com>
2025-04-23 17:08:29 +0200

    fix translate

--- src/components/CVModal.jsx
+++ src/components/CVModal.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
 import { theme } from "../helpers/theme";
+import { useTranslation } from "react-i18next";
 
 const CVModal = ({ isOpen, onClose }) => {
+  const { t } = useTranslation();
   if (!isOpen) return null;
 
   return (
@@ -20,33 +22,21 @@ const CVModal = ({ isOpen, onClose }) => {
               />
             </div>
 
-            <h3 className="text-xl md:text-2xl font-semibold mb-2">Formación:</h3>
+            <h3 className="text-xl md:text-2xl font-semibold mb-2">{t("home.fundador.cv.titleFormacion")}</h3>
             <ul className="flex flex-col gap-y-2">
-              <li>Licenciado en Ciencias de la Actividad Física y del Deporte por la Universidad Central del Ecuador Colegiado 52.5001.</li>
-              <li>Periodista diplomado por La Academia Europea de Periodismo en España año 2025.</li>
-              <li>Entrenador de Musculación por Centro Internacional de Profesorado Deportivo LIFE año 2000 Madrid.</li>
-              <li>Maestro multidisciplinar campeón Internacional de Sur América, tiene 6 Cinturones Negros 5to Dan en:</li>
-              <li>Taekwondo - Hapkido - Kick Boxing - Judo - Full Contact - Sambo.</li>
-              <li>Reconocido a nivel mundial por las Federaciones: WTF, ITF, WHF, WAKO, FIDAM, FET.</li>
-              <li>Curso de Socorismo y Primeros Auxilios por la Cruz Roja Española, BCN 2025. </li>
+              {t("home.fundador.cv.listFormacion", { returnObjects: true }).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
 
-
-            <h3 className="text-xl md:text-2xl font-semibold my-2">Experiencia:</h3>
+            <h3 className="text-xl md:text-2xl font-semibold my-2">{t("home.fundador.cv.titleExperiencia")}</h3>
             <ul className="flex flex-col gap-y-2">
-              <li>Director Técnico y Profesor de varios centros deportivos en España y Ecuador.</li>
-              <li>Entrenador personal de deportistas de élite, celebrities, modelos y personas que quieren llevar una vida sana y saludable.</li>
-              <li>Experto en Rendimiento Deportivo y oposiciones para Policías, Bomberos, Guardias de Seguridad.</li>
-              <li>Médico Acupunturista y Digito-puntura Diplomado en &quot;Center Acupuntura Kee Wong Youg&quot; Barcelona 2004.</li>
-              <li>Dietista y Nutricionista experto asesor para Culturistas profesionales y amateur.</li>
-              <li>Autor de 34 libros publicados en España y en Amazon a nivel mundial.</li>
-              <li>Entrenador Personal y creador del &quot;Sistem Power&quot; La transformación metamorfosis de mente, cuerpo, y espíritu.</li>
-              <li>Productor y periodista colegiado SPC - SPM - FEP - FIP - AEP de varios medios de comunicación.</li>
-              <li>Terapeuta tradicional Andino y Motivador personal PNL. Conferencista y Catedrático educacional en universidades como: CAMU, UCM, UCE, PUSE, UAB, etc.</li>
-
+              {t("home.fundador.cv.listExperiencia", { returnObjects: true }).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
-            <div className={`${theme.primary} w-[240px] mx-auto mt-4 p-2 rounded-lg`}>
-              <a href="https://en.wikipedia.org/wiki/Amaruk_Kayshapanta" target="_blank" >Más Información en Wikipedia</a>
+            <div className={`${theme.primary} w-fit mx-auto mt-4 p-2 rounded-lg`}>
+              <a href="https://en.wikipedia.org/wiki/Amaruk_Kayshapanta" target="_blank" >{t("home.fundador.cv.masInfoBtn")}</a>
             </div>
 
           </div>
